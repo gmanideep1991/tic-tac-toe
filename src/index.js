d7d12c7c3afde2a9a22a8b3032370e8d6c813bb9
Gattamaneni <MG044502@cerner.net>
2020-01-10 12:48:38 -0500

    intial game

--- src/index.js
+++ src/index.js
@@ -1,12 +1,92 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      isUserX: true
+    };
+  }
+  createSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.isUserX ? "X" : "O";
+    this.setState({ squares: squares, isUserX: !this.state.isUserX });
+  }
+  render() {
+    let winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner is " + winner;
+    } else {
+      status = "Next Player is: " + (this.state.isUserX ? "X" : "O");
+    }
+    return (
+      <div className="board">
+        <div>{status}</div>
+        <div className="board-row">
+          {this.createSquare(0)}
+          {this.createSquare(1)}
+          {this.createSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.createSquare(3)}
+          {this.createSquare(4)}
+          {this.createSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.createSquare(6)}
+          {this.createSquare(7)}
+          {this.createSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+class Game extends React.Component {
+  render() {
+    return <Board />;
+  }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+ReactDOM.render(<Game />, document.getElementById("root"));
